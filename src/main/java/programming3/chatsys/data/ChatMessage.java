7a64168b54160b78dcb6b48ef0ba61b927da714b
kkddyz <2387811469@qq.com>
2020-11-07 20:43:33 +0800

    Initial commit

---
+++ src/main/java/programming3/chatsys/data/ChatMessage.java
@@ -0,0 +1,14 @@
+package programming3.chatsys.data;
+
+import com.sun.jmx.snmp.Timestamp;
+
+/**
+ * Create the class with its attributes, getter methods and constructors to initialize
+ * them. Also implement the toString, equals and hashCode methods. You can use IntelliJ to generate those for
+ * you.
+ */
+public class ChatMessage {
+    private int id;
+    private String username;
+    private Timestamp timestamp;
+}
